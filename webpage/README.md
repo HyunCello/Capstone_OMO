2a1479582f3234fa6848a94b7bce45fb05363369
HyunCello <jeremykim95@gmail.com>
2020-04-07 14:40:18 +0900

    웹페이지 구조 제작

---
+++ webpage/README.md
@@ -0,0 +1,5 @@
+# 서비스 제공을 위한 웹페이지 만들기
+
+## Google firebase를 이용한 배포
+
+## Naver api 중 map을 이용해 맵에 표시
---
+++ webpage/public/function.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function () { 
+    $("#gotomain").on("click", function () { 
+        location.href="index.html" 
+    }); 
+    $("#gotoorder").on("click", function () {
+        location.href="order.html" 
+    });
+    $("#gotomap").on("click", function () {
+        location.href="map.html" 
+    });
+    $("#gotopassword").on("click", function () {
+        location.href="password.html" 
+    });
+    $("#gotoorder").on("click", function () {
+        location.href="order.html" 
+    });
+
+
+  }); 
