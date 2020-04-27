564b092e4c10f1ed2200493924875a658b35974c
HyunCello <jeremykim95@gmail.com>
2020-04-27 23:44:09 +0900

    add db

--- webpage/public/function.js
+++ webpage/public/function.js
@@ -32,3 +32,16 @@ $(document).ready(function () {
     });
 }); 
 
+var express = require('express');
+var router = express.Router();
+ 
+/* GET home page. */
+router.get('/',function(req, res, next) {
+  res.render('index', { title:'Express' });
+});
+ 
+router.get('/test',function(req, res, next) {
+      res.render('test');
+});
+ 
+module.exports = router;
