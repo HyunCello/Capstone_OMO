2bdf45f88d50d62e404de55ee72518be0cb03c0c
HyunCello <jeremykim95@gmail.com>
2020-05-29 01:20:18 +0900

    modal창 만들기 에제

--- webpage/public/js/function.js
+++ webpage/public/js/function.js
@@ -58,4 +58,13 @@ $(document).ready(function () {
     location.href = "html/test.html";
   });
 });
+
 //////////////////////////////////////////////////////
+
+document.getElementById("modal_open_btn").onclick = function () {
+  document.getElementById("modal").style.display = "block";
+};
+
+document.getElementById("modal_close_btn").onclick = function () {
+  document.getElementById("modal").style.display = "none";
+};
